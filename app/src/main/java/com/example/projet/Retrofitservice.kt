96742da926b3d73c428dcdbb11084f03ec6519dd
kamelzak <gk_zaknoune@esi.dz>
2020-03-06 15:50:24 +0000

    MedTrait2

--- app/src/main/java/com/example/projet/Retrofitservice.kt
+++ app/src/main/java/com/example/projet/Retrofitservice.kt
@@ -8,7 +8,7 @@ object Retrofitservice {
 
     var gson = GsonBuilder().setLenient().create()
     val endpoint :Endpoint by lazy {
-        Retrofit.Builder().baseUrl("https://74ec6daa.ngrok.io/").
+        Retrofit.Builder().baseUrl("https://04c766a4.ngrok.io/").
             addConverterFactory(GsonConverterFactory.create(gson)).
             build().create(Endpoint::class.java)
     }
